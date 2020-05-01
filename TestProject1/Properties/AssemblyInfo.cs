a774eb9b55cc80c1992cce120bcb65f53423c846
Yumer Adem Yumer <yuy@tdc.dk>
2020-05-01 12:02:08 +0200

    add website and tests

---
+++ TestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb8bbb8a-1c27-4e42-9bf5-67e139c72d36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestInitialize]
+        public void Intialize()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44374");
+
+        }
+
+        [TestMethod]
+        public void ExecuteTest2()
+        {
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("Contact")).Click();
+            Thread.Sleep(2000);
+            return;
+        }
+
+        [TestCleanup]
+        public void EndTest()
+        {
+            //close the browser  
+            driver.Close();
+        }
+    }
+}
